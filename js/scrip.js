5a708ab3b940a883ef5c112a0fc519aa97fef507
karol3012 <kjarevalope@ufpso.edu.co>
2024-05-24 15:12:08 -0500

    load parcial

---
+++ js/scrip.js
@@ -0,0 +1,10 @@
+
+var ase_one = document.getElementById("ase_one").value;
+var ase_one = document.getElementById("ase_two").value;
+
+function limpiar(){
+    document.getElementById("ase_one").value="";
+    document.getElementById("ase_two").value="";
+    document.getElementById("ase_three").value="";
+    document.getElementById("ase_four").innerText="";
+  }
